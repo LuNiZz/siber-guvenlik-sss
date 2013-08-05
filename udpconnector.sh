338da08635be5ba75a55b3218c414eb90f69b4aa
theblaxx <can.deger@gmail.com>
2013-08-05 07:44:32 +0300

    Update udpconnector.sh

--- udpconnector.sh
+++ udpconnector.sh
@@ -2,6 +2,8 @@
   
 for I in {1..65535}; do
   #echo $I 
-  #sudo hping3 -E /tmp/ovpn.trigger -d 14 -2 -p $I -c 1 --fast 81.145.54.190 
-  cat /tmp/ovpn.trigger | ncat -u --send-only 81.145.54.190 $I 
+  #sudo hping3 -E /tmp/ovpn.trigger -d 14 -2 -p $I -c 1 --fast 280.198.66.190 
+  cat /tmp/ovpn.trigger | ncat -u --send-only 280.198.66.190 $I 
 done; 
+#note that target IP address is not a real one. And hping also works but i prefer ncat.
+#ovpn.trigger is the capture file.
